33c3f0d4885155ca676b10f7febe9843da0243bc
Joël Stemmer <stemmertech@gmail.com>
2019-10-06 17:44:31 +0100

    gtr: Add default package name to report

--- go-junit-report.go
+++ go-junit-report.go
@@ -15,7 +15,7 @@ var (
 	packageName   = flag.String("package-name", "", "specify a package name (compiled test have no package name in output)")
 	goVersionFlag = flag.String("go-version", "", "specify the value to use for the go.version property in the generated XML")
 	setExitCode   = flag.Bool("set-exit-code", false, "set exit code to 1 if tests failed")
-	printEvents = flag.Bool("print-events", false, "print events (for debugging)")
+	printEvents   = flag.Bool("print-events", false, "print events (for debugging)")
 )
 
 func main() {
@@ -39,7 +39,7 @@ func main() {
 			fmt.Printf("%02d: %#v\n", i, ev)
 		}
 	}
-	report := gtr.FromEvents(events)
+	report := gtr.FromEvents(events, *packageName)
 
 	if !*noXMLHeader {
 		fmt.Fprintf(os.Stdout, xml.Header)
